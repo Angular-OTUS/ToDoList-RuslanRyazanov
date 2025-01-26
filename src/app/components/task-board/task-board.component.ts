0902a32dcee908d50d1617ceaa079ad934261530
Ruslan Ryazanov <Snow88ilike@gmail.com>
2025-01-26 23:24:00 +0300

    homework-8

---
+++ src/app/components/task-board/task-board.component.ts
@@ -0,0 +1,45 @@
+import { Component } from '@angular/core';
+import { Router, RouterLink, RouterOutlet } from "@angular/router";
+import { NgClass } from "@angular/common";
+
+@Component({
+  selector: 'app-task-board',
+  standalone: true,
+  imports: [
+    RouterLink,
+    NgClass,
+    RouterOutlet
+  ],
+  templateUrl: './task-board.component.html',
+  styleUrl: './task-board.component.scss'
+})
+export class TaskBoardComponent {
+  protected readonly componentTitle: string = 'Board';
+
+  constructor(
+    private router: Router
+  ) {}
+
+  protected getCurrentTabName(): string | undefined {
+    const currentPath: string = this.router.url.split('/').filter(Boolean).join('/');
+    const route = this.routes
+      .find(route => currentPath.startsWith(route.path));
+
+    return route?.name;
+  }
+
+  protected isCurrentRoute(route: string): boolean {
+    return this.getCurrentTabName() === route;
+  }
+
+  protected readonly routes = [
+    {
+      name: 'BACKLOG',
+      path: 'backlog/tasks'
+    },
+    {
+      name: 'BOARD',
+      path: 'board'
+    }
+  ];
+}
